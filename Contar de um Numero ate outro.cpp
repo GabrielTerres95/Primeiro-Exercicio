9b26fc093c6c06d5db4b923a59a47aa1d10d7930
GabrielTerres95 <90807218+GabrielTerres95@users.noreply.github.com>
2022-05-15 19:34:59 -0300

    first commit

---
+++ Contar de um Numero ate outro.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+int i;
+for (i = 1;i <= 10;i++)    //Podemos mudar aqui no laço de repetição para contar até o numero que deseja 
+printf("%d\n",i);
+getch();
+
+//Muito obrigado por ler o código
+}
